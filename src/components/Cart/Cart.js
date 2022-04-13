7e37f3be1ae42ccbf72bfddfa3a672e97b0fde6d
alperkilickaya <alperkilickaya@gmail.com>
2022-04-14 00:41:36 +0300

    updated cart items and amounts

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
-  console.log(props);
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+  const cartItemRemoveHandler = (id) => {};
+  const cartItemAddHandler = (item) => {};
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
-        <li key={item.id}>{item.name}</li>
+      {cartCtx.items.map((item) => (
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          //onRemove={() => cartItemRemoveHandler(item.id)}
+          //onAdd={() => cartItemAddHandler(item)}
+          onRemove={cartItemRemoveHandler.bind(null, item.id)}
+          onAdd={cartItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -15,14 +30,14 @@ const Cart = (props) => {
     <Modal onClose={props.onClose}>
       {cartItems}
       <div className={classes.total}>
-        <span>Total Amount:</span>
-        <span>35.62</span>
+        <span>Total Amount</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
         <button className={classes["button--alt"]} onClick={props.onClose}>
           Close
         </button>
-        <button className={classes.button}>Order</button>
+        {hasItems && <button className={classes.button}>Order</button>}
       </div>
     </Modal>
   );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -12,14 +12,41 @@ const cartReducer = (state, action) => {
     // burada oluşan updatedItems item objelerini tutan bir array. [{id: 'm1', name: 'Sushi', price: 22.99, amount: 1},
     // id: 'm2', name: 'Schnitzel', price: 16.5, amount: 1},
     // {id: 'm1', name: 'Sushi', price: 22.99, amount: 1}]
-    const updatedItems = state.items.concat(action.payload);
-    console.log("updatedItems", updatedItems);
+
+    //action payload ile gelen item'in id'sini kullanarak state.items array'inde index'ini buluyoruz.
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.payload.id
+    );
+
+    // index sayesinde mevcut state'de yer alan ve tekrar gelen item'ı buluyoruz.
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedItems;
+
+    // eğer aynı item mevcutsa
+    if (existingCartItem) {
+      // state'de yer alan item'ın amount'ını arttırıp geri kalan propertilerini muhafaza ediyoruz.
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.payload.amount,
+      };
+      // mevcut items'ın kopyasını oluşturuyoruz. Bu immutablility'i sağlıyor.
+      updatedItems = [...state.items];
+      // aynı item'ı yeni gelen ile override ediyoruz.
+      updatedItems[existingCartItemIndex] = updatedItem;
+      // eğer action payload'dan gelen yeni bir item ise
+    } else {
+      // yeni item'ı state'deki items'a ekliyoruz.
+      updatedItems = state.items.concat(action.payload);
+    }
+
     // her item gönderildiğinde updatedTotalAmount, context'te tutulan state.totalAmount'a eklenerek tekrar hesaplanır.
     // state.totalAmount initial olarak 0. Her item eklendiğinde bir önceki totalAmount'a eklenir ve bu değer
     // state'de saklanır. Bir sonraki item gönderildiğinde state'de saklanan son değer ile tekrar toplanır.
     const updatedTotalAmount =
       state.totalAmount + action.payload.price * action.payload.amount;
     //reducer mevcut state'i aldı ve aşağıda yeni state'i döndü.
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
